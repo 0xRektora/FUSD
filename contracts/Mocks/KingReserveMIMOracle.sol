9dd4661ed55b55ee2cf769d05372d0147d0fb3a4
Sadek Walid Mendi <Bloodyline01@gmail.com>
2022-01-19 17:08:14 -0500

    Fixed typing bug

--- contracts/Mocks/KingReserveMIMOracle.sol
+++ contracts/Mocks/KingReserveMIMOracle.sol
@@ -6,6 +6,6 @@ pragma solidity ^0.8.0;
 
 contract KingReserveMIMOracle {
     function getExchangeRate(uint256 amount) external pure returns (uint256) {
-        return (amount * 10) / 100 + amount;
+        return amount - (amount * 10) / 100;
     }
 }
