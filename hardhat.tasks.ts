ae210cef00e1e8d4a464b355de8a28fdd737f052
Sadek Walid Mendi <Bloodyline01@gmail.com>
2022-01-19 16:14:12 -0500

    Updated test task

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -11,11 +11,11 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 
 task('run-mainnet-test', 'Open allowance', async (taskArgs, hre) => {
   const king = await hre.ethers.getContractAt('King', (await hre.deployments.get('King')).address);
-  const wusd = await hre.ethers.getContractAt('WUSD', (await hre.deployments.get('WUSD')).address);
+  const fusd = await hre.ethers.getContractAt('FUSD', (await hre.deployments.get('FUSD')).address);
   const mim = await hre.ethers.getContractAt('ERC20', '0x82f0B8B456c1A451378467398982d4834b6829c1');
 
   const eth = hre.ethers.utils.parseEther('1');
 
-  await (await wusd.approve(king.address, eth)).wait();
+  await (await fusd.approve(king.address, eth)).wait();
   await (await mim.approve(king.address, eth)).wait();
 });
